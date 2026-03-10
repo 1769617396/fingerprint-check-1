cbcaaf2b8ec34e1104ba84e72c84be68302f502d
1769617396 <71252657+1769617396@users.noreply.github.com>
2026-03-10 11:37:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,112 @@
-function initWebGL() {
-
+// ================== WebGL 部分 ==================
+function createWebGLContext() {
     const canvas = document.createElement("canvas");
-
-    const gl =
-        canvas.getContext("webgl") ||
-        canvas.getContext("experimental-webgl");
-
-    if (!gl) {
-
-        document.body.innerHTML +=
-            "<p>WebGL not supported</p>";
-
-        return;
-    }
-
-    showBasicInfo(gl);
-    showGPUInfo(gl);
-    showCapabilities(gl);
-    showExtensions(gl);
-    generateFingerprint(gl);
-}
-
-function showBasicInfo(gl) {
-
-    const info = {
-        version: gl.getParameter(gl.VERSION),
-        shading: gl.getParameter(gl.SHADING_LANGUAGE_VERSION),
-        vendor: gl.getParameter(gl.VENDOR),
-        renderer: gl.getParameter(gl.RENDERER)
-    };
-
-    renderTable("webgl-basic", info);
-}
-
-function showGPUInfo(gl) {
-
-    const ext = gl.getExtension("WEBGL_debug_renderer_info");
-
-    if (!ext) {
-
-        document.getElementById("webgl-gpu").innerText =
-            "WEBGL_debug_renderer_info not available";
-
-        return;
-    }
-
-    const gpu = {
-        vendor: gl.getParameter(ext.UNMASKED_VENDOR_WEBGL),
-        renderer: gl.getParameter(ext.UNMASKED_RENDERER_WEBGL)
-    };
-
-    renderTable("webgl-gpu", gpu);
-}
-
-function showCapabilities(gl) {
-
-    const caps = {
-        MAX_TEXTURE_SIZE: gl.getParameter(gl.MAX_TEXTURE_SIZE),
-        MAX_VERTEX_ATTRIBS: gl.getParameter(gl.MAX_VERTEX_ATTRIBS),
-        MAX_RENDERBUFFER_SIZE: gl.getParameter(gl.MAX_RENDERBUFFER_SIZE)
-    };
-
-    renderTable("webgl-cap", caps);
-}
-
-function showExtensions(gl) {
-
-    const ext = gl.getSupportedExtensions();
-
-    document.getElementById("webgl-ext").innerHTML =
-        "<pre>" + JSON.stringify(ext, null, 2) + "</pre>";
-}
-
-function generateFingerprint(gl) {
-
-    const data = [
-        gl.getParameter(gl.VERSION),
-        gl.getParameter(gl.RENDERER),
-        gl.getParameter(gl.VENDOR)
-    ].join("###");
-
-    const hash = simpleHash(data);
-
-    document.getElementById("webgl-fp").innerText = hash;
+    return canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 }
 
 function renderTable(id, obj) {
-
     let html = "<table>";
-
     for (let key in obj) {
-
-        html +=
-            "<tr><td>" + key + "</td><td>" + obj[key] + "</td></tr>";
+        html += `<tr><td>${key}</td><td>${obj[key]}</td></tr>`;
     }
-
     html += "</table>";
-
     document.getElementById(id).innerHTML = html;
 }
 
+function showWebGLInfo(gl) {
+    // 基本信息
+    const basicInfo = {
+        version: gl.getParameter(gl.VERSION),
+        shadingLanguage: gl.getParameter(gl.SHADING_LANGUAGE_VERSION),
+        vendor: gl.getParameter(gl.VENDOR),
+        renderer: gl.getParameter(gl.RENDERER)
+    };
+    renderTable("webgl-basic", basicInfo);
+
+    // GPU info
+    const debugInfo = gl.getExtension("WEBGL_debug_renderer_info");
+    const gpuInfo = debugInfo ? {
+        unmaskedVendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+        unmaskedRenderer: gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL)
+    } : {unmaskedVendor: "N/A", unmaskedRenderer: "N/A"};
+    renderTable("webgl-gpu", gpuInfo);
+
+    // WebGL capabilities
+    const caps = {
+        MAX_TEXTURE_SIZE: gl.getParameter(gl.MAX_TEXTURE_SIZE),
+        MAX_VERTEX_ATTRIBS: gl.getParameter(gl.MAX_VERTEX_ATTRIBS),
+        MAX_RENDERBUFFER_SIZE: gl.getParameter(gl.MAX_RENDERBUFFER_SIZE),
+        MAX_VIEWPORT_DIMS: gl.getParameter(gl.MAX_VIEWPORT_DIMS).toString()
+    };
+    renderTable("webgl-cap", caps);
+
+    // Supported extensions
+    const extensions = gl.getSupportedExtensions();
+    document.getElementById("webgl-ext").innerHTML = "<pre>" + JSON.stringify(extensions, null, 2) + "</pre>";
+
+    // Fingerprint
+    const fpData = [
+        gl.getParameter(gl.VERSION),
+        gl.getParameter(gl.SHADING_LANGUAGE_VERSION),
+        gl.getParameter(gl.VENDOR),
+        gl.getParameter(gl.RENDERER),
+        extensions.join(",")
+    ].join("###");
+    document.getElementById("webgl-fp").innerText = simpleHash(fpData);
+}
+
+// ================== Canvas Fingerprint 部分 ==================
+function generateCanvasFingerprint() {
+    const canvas = document.createElement("canvas");
+    canvas.width = 200;
+    canvas.height = 50;
+    const ctx = canvas.getContext("2d");
+
+    // 渲染文本和图形
+    ctx.fillStyle = "#f60";
+    ctx.fillRect(0, 0, 200, 50);
+    ctx.fillStyle = "#069";
+    ctx.font = "20px Arial";
+    ctx.fillText("Hello WebGL + Canvas!", 2, 30);
+
+    // 获取 Base64 数据
+    const dataURL = canvas.toDataURL();
+
+    // 哈希
+    const hash = simpleHash(dataURL);
+
+    // 模拟厂商信息（Canvas本身没有vendor，只能通过 hash 模拟）
+    const simulatedVendor = "CanvasRenderer";
+    const simulatedRenderer = "Canvas 2D";
+
+    // 输出
+    const canvasInfo = {
+        simulatedVendor,
+        simulatedRenderer,
+        fingerprintHash: hash
+    };
+
+    renderTable("canvas-fp", canvasInfo);
+}
+
+// ================== 简单 Hash 函数 ==================
 function simpleHash(str) {
-
     let hash = 0;
-
     for (let i = 0; i < str.length; i++) {
-
         hash = ((hash << 5) - hash) + str.charCodeAt(i);
-
-        hash |= 0;
+        hash |= 0; // 转 32 位有符号整数
     }
-
     return hash;
 }
 
-window.onload = initWebGL;
+// ================== 初始化 ==================
+window.onload = function() {
+    const gl = createWebGLContext();
+    if (!gl) {
+        alert("WebGL not supported");
+        return;
+    }
+
+    showWebGLInfo(gl);
+    generateCanvasFingerprint();
+};
